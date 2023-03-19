5daef149d503533358dd2867e9f88299949b1ca2
nanato12 <admin@okj.info>
2023-03-20 02:20:52 +0900

    update: .php-cs-fixer.dist.php

--- app/.php-cs-fixer.dist.php
+++ app/.php-cs-fixer.dist.php
@@ -1,7 +1,12 @@
 <?php
 
 $finder = PhpCsFixer\Finder::create()
-    ->exclude('database/migrations')
+    ->exclude(
+        [
+            'database/migrations',
+            'config',
+        ]
+    )
     ->in(__DIR__);
 
 $config = new PhpCsFixer\Config();
@@ -9,7 +14,45 @@ return $config->setRules(
     [
         '@PSR12' => true,
         '@PhpCsFixer' => true,
-        'strict_param' => true,
         'array_syntax' => ['syntax' => 'short'],
+        'blank_line_before_statement' => [
+            'statements' => [
+                'break',
+                'case',
+                'continue',
+                'declare',
+                'default',
+                'do',
+                'exit',
+                'for',
+                'foreach',
+                'goto',
+                'if',
+                'include',
+                'include_once',
+                'require',
+                'require_once',
+                'return',
+                'switch',
+                'throw',
+                'try',
+                'while',
+                'yield',
+            ],
+        ],
+        'self_static_accessor' => true,
+        'global_namespace_import' => [
+            'import_constants' => true,
+            'import_functions' => true,
+        ],
+        'multiline_whitespace_before_semicolons' => ['strategy' => 'no_multi_line'],
+        'phpdoc_no_empty_return' => false,
+        'single_quote' => true,
+        'strict_param' => true,
+        'yoda_style' => [
+            'always_move_variable' => false,
+            'equal' => false,
+            'identical' => false
+        ],
     ]
 )->setFinder($finder);
