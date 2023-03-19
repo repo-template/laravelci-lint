e3993305194d162fb2b149b97358c7d0c58e18d0
nanato12 <admin@okj.info>
2023-03-20 01:54:39 +0900

    fix: by php-cs-fixer

--- app/app/Exceptions/Handler.php
+++ app/app/Exceptions/Handler.php
@@ -10,19 +10,17 @@ class Handler extends ExceptionHandler
     /**
      * A list of exception types with their corresponding custom log levels.
      *
-     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
+     * @var array<class-string<Throwable>, \Psr\Log\LogLevel::*>
      */
     protected $levels = [
-        //
     ];
 
     /**
      * A list of the exception types that are not reported.
      *
-     * @var array<int, class-string<\Throwable>>
+     * @var array<int, class-string<Throwable>>
      */
     protected $dontReport = [
-        //
     ];
 
     /**
@@ -42,7 +40,6 @@ class Handler extends ExceptionHandler
     public function register(): void
     {
         $this->reportable(function (Throwable $e) {
-            //
         });
     }
 }
